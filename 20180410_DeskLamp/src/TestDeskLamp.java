3190f439a7fe280a997e474158df76c511b0a55d
2015440129 <dkdlrhtkfl@naver.com>
2018-04-10 17:43:57 +0900

    '20180410'

---
+++ 20180410_DeskLamp/src/TestDeskLamp.java
@@ -0,0 +1,16 @@
+
+public class TestDeskLamp {
+
+	public static void main(String[] args) {
+		DeskLamp deskLamp = new DeskLamp();
+		deskLamp.turnOn();
+		
+		System.out.println(deskLamp);
+		
+		deskLamp.turnOff();
+		
+		System.out.println(deskLamp);
+		
+	}
+
+}
