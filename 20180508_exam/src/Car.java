f1c50b28dfa3d1de416b17da4d4a5b933f800527
2015440129 <dkdlrhtkfl@naver.com>
2018-05-15 15:03:04 +0900

    20180508

---
+++ 20180508_exam/src/Car.java
@@ -0,0 +1,11 @@
+
+public class Car implements Drivable {
+	@Override
+	public void brake() {
+		System.out.println("ºê·¹ÀÌÅ©¸¦ ¹â´Ù");		
+	}
+	@Override
+	public void acc() {
+		System.out.println("¾×¼¿À» ¹â´Ù");
+	}
+}
---
+++ 20180508_exam/src/Test.java
@@ -0,0 +1,13 @@
+
+public class Test {
+
+	public static void main(String[] args) {
+		Car newCar = new Car();
+		newCar.brake();
+		newCar.acc();
+		Bicycle newBicycle = new Bicycle();
+		newBicycle.brake();
+		newBicycle.acc();		
+	}
+
+}
